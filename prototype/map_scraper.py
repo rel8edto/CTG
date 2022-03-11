3796f509c9afd1ae182bb7b102f1315c850b1967
Konscious7 <nathanieljacques@gmail.com>
2022-03-11 11:06:37 -0500

    updated to the map api

--- prototype/map_scraper.py
+++ prototype/map_scraper.py
@@ -1,12 +1,15 @@
-from PIL import Image, ImageFile
-from config.credentials import GOOGLE_API_KEY, MAP_BOX_API_KEY
+# Import needed libraries
+from PIL import Image, ImageFile, UnidentifiedImageError
+from itsdangerous import exc
+from config.credentials import GOOGLE_API_KEY
+from tqdm import trange
 from io import BytesIO
 import pandas as pd
 import requests
 import os
 
 
-def map_api(filename: str, zoom: int, map_api: str):
+def map_api(filename: str, zoom: int, map_api: str, num_images: int):
     """
     Summary:
         This image scraper is used to build the training and test data for any CNN model by extracting images from google or mapbox images. 
@@ -15,60 +18,64 @@ def map_api(filename: str, zoom: int, map_api: str):
         filename (str): name of the directory where cooridantes are located
         zoom (int): The zoom level of the map
         map_api (str): Determine which api to use between google='google' and mapbox='mapbox'
+        num_images (int): The number of images that will be returned
     """
     
     # Check the name of file
     file_name = os.path.basename(filename)
     sub_dir = os.path.splitext(file_name)[0]
+    print("This is the fil")
 
-    # Create directory if it doesn't exist
-    isExist = os.path.exists('CTG/prototype/train_data/' + sub_dir)
-
-    if not isExist:
-        os.makedirs('CTG/prototype/train_data/' + sub_dir)
     
     # Iterate through csv files and convert to pandas format
     df = pd.read_csv(filename)
-    print(df)
-    
-    for idx in range(len(df)):
 
-        latitude = df['latitude'][idx]
-        longitude = df['Longitude'][idx]
-        company_name = df['name'][idx]
-        print(longitude, latitude, company_name)
+    # create a counter for num of images
+    counter = 0
 
-        if map_api == 'google':
-            # Connect to API server
-            res_google = requests.get(
-                f'https://maps.googleapis.com/maps/api/staticmap?center={latitude},{longitude}&zoom={zoom}&size=600x600&maptype=satellite&key={GOOGLE_API_KEY}')
-            
-            # # Truncate the png files 
-            # ImageFile.LOAD_TRUNCATED_IMAGES = True
-            
-            with open(f"{company_name}.png", "wb") as f:
-               
-                f.write(res_google.content)
-    
-                image = Image.open(f"{company_name}.png")
-                # image.save(f'CTG/prototype/train_data/{sub_dir}/{company_name + ".png"}','png')
-                image.show(f"{company_name}.png")
-    
-        elif map_api == 'mapbox':
-            res_map_box = requests.get(
-                f'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/{longitude},{latitude},{zoom}/600x600?access_token={MAP_BOX_API_KEY}')
+    while counter < num_images:
 
-            with open("gfg.png", "wb") as f:
-                f.write(res_map_box.content)
-    
-                image = Image.open("gfg.png")
-                image.save(f'CTG/prototype/train_data/{sub_dir}/{company_name + ".png"}', 'png')
+        for idx in trange(len(df), desc="Processing images"):
 
-        else:
-            print("Please specify static map API server")
+            latitude = df['latitude'][idx]
+            longitude = df['longitude'][idx]
+            company_name = df['name'][idx]
+            sub_category = df['FeatureSubType'][idx]
+            # print(longitude, latitude, company_name)
+
+            # Create directory if it doesn't exist
+            isExist = os.path.exists('/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/img' + sub_category)
+
+            if not isExist:
+                os.makedirs('/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/img' + sub_category)
+
+            if map_api == 'google':
+                    # Connect to API server
+                    res_google = requests.get(
+                        f'https://maps.googleapis.com/maps/api/staticmap?center={latitude},{longitude}&zoom={zoom}&size=300x300&maptype=satellite&key={GOOGLE_API_KEY}')
 
 
+                    with open(f"{company_name}.png", "wb") as f:
+                        f.write(res_google.content)
+
+                        try:
+                            image = Image.open(f"{company_name}.png")
+                            image.save(f'/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/img/{sub_category}/{company_name + ".png"}', 'png')
+                            # image.show(f"{company_name}.png")
+                        except:
+                            print(sub_category)
+                            print(company_name + ".png")
+                            image.save(
+                                f'/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/reject/{company_name + ".png"}', 'png')
+                    counter += 1
+
+            if counter == num_images:
+                print("images Proccessing completed....")
+                break 
+
+        
 if __name__ == '__main__':
-    # corp = map_api('CTG/prototype/train_data/corporatebuilding.csv', 21, 'google')
-    manufact = map_api('CTG/prototype/train_data/corporatebuilding.csv', 26, 'google')
+    neo4j_emergency = map_api('/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/neo4j_emergency.csv',18, 'google', num_images=100)
+    # transportation = map_api('/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/Transportation.csv', 18, 'google', num_images=350)
+    # emergency = map_api('/Users/nathanieljacques/Rel8ed.to/CTG/prototype/data/CentersOfWorship.csv', zoom=18, map_api='google', num_images=350)
     
